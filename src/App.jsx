5cf71d57d63b839eabd16d6ad7421a488ed484b7
nutan kumari <nutananju240@gmail.com>
2024-02-26 11:45:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import { Box } from '@mui/material'
+
+function App() {
+  
+
+  return (
+    <>
+   <Box>hello</Box>
+    </>
+  )
+}
+
+export default App
